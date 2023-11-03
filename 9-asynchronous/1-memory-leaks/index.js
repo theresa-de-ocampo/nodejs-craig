122d43c81ef5e440e7b62b96f3604b8e431b9094
theresa-de-ocampo <ma_theresa7@yahoo.com>
2023-11-03 15:32:12 +0800

    Structure clean-up

--- 9-asynchronous/1-memory-leaks/index.js
+++ 9-asynchronous/1-memory-leaks/index.js
@@ -27,24 +27,3 @@
  *
  * https://auth0.com/blog/four-types-of-leaks-in-your-javascript-code-and-how-to-get-rid-of-them/
  */
-
-// Failing to terminate an asynchronous function after callback
-
-function wait(ms, callback) {
-  ms = parseFloat(ms);
-
-  if (!ms || ms < 1 || ms > 3000) {
-    const err = new RangeError("Invalid ms value");
-    callback(err, ms);
-    return;
-  }
-
-  setTimeout(callback, ms, null, ms);
-}
-
-wait(0, (err, ms) => {
-  if (err) console.log(err);
-  else console.log(`Waited for ${ms}ms`);
-
-  // console.log(`Waited for ${ms}ms`);
-});
